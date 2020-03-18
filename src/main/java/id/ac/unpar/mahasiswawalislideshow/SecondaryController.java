984d84b3e49ff093d10d3a7ff6811c81db9acc66
Pascal Alfadian Nugroho <pascal@unpar.ac.id>
2020-03-18 09:23:09 +0700

    Initial files
    From NetBeans > New Project > Java with Maven > JavaFX Maven Archetype

---
+++ src/main/java/id/ac/unpar/mahasiswawalislideshow/SecondaryController.java
@@ -0,0 +1,12 @@
+package id.ac.unpar.mahasiswawalislideshow;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module id.ac.unpar.mahasiswawalislideshow {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens id.ac.unpar.mahasiswawalislideshow to javafx.fxml;
+    exports id.ac.unpar.mahasiswawalislideshow;
+}
